16b36086530d938571095ddb4e18c4ae58518d4a
Musaffarul Islam <musaffarulislam@gmail.com>
2025-09-07 13:10:42 +0530

    feat: try to fix build issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Completely disable Next.js image optimization
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
