19f03b6ce3740fd4ed27058433e82067c7ba281b
Musaffarul Islam <musaffarulislam@gmail.com>
2025-09-07 13:41:14 +0530

    feat: contact form

--- src/components/homePage/ContactCTA.tsx
+++ src/components/homePage/ContactCTA.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import Image from "next/image";
-import ContactImg from "@/assets/background-image3.jpg"; // replace with your image
+import ContactImg from "@/assets/background-image3.jpg";
 import ContactForm from "./ContactForm";
 
 export default function ContactSection() {
   return (
     <section id="contact" className="py-20 bg-card text-foreground">
-      <div className="container mx-auto px-6 grid md:grid-cols-2 gap-10 items-center">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row gap-10 items-stretch">
         {/* Left Side - Image */}
-        <div className="hidden md:block h-full">
-          <Image
-            src={ContactImg}
-            alt="Contact Border Link"
-            className="h-full w-full rounded-xl shadow-lg object-cover"
-          />
+        <div className="hidden md:flex flex-1 relative">
+          <div className="absolute inset-0">
+            <Image
+              src={ContactImg}
+              alt="Contact Border Link"
+              className="w-full h-full rounded-xl shadow-lg object-cover"
+              fill
+            />
+          </div>
         </div>
 
-        <ContactForm />
+        {/* Right Side - Form */}
+        <div className="flex-1">
+          <ContactForm />
+        </div>
       </div>
     </section>
   );
